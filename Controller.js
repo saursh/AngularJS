ebbffa3c151458b961b8cb73cb31539fb2d6d884
Saurabh Shrivastava <saurabhs83@gmail.com>
2018-05-28 00:09:59 -0700

    Controller with data binding

--- Controller.js
+++ Controller.js
@@ -1,3 +1,11 @@
 var MainController = function($scope) {
+   
+    var person ={
+        firstName:"Saurabh",
+        lastName:"Shrivastava",
+        image: "https://scontent.fsnc1-1.fna.fbcdn.net/v/t1.0-1/c0.4.320.320/p320x320/29595415_10156451274156802_8113962827254233873_n.jpg?_nc_cat=0&oh=7615bf8f704f4dbc216cfae8bebf0e37&oe=5BBA0793"
+    };
     $scope.message="hello how are you?";
+    $scope.person=person;
+
 };
